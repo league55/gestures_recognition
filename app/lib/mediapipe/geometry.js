e6f8ae2720a8b4bf473cb75e5250442f8bb22c8a
mrudenko <m.rudenko@zaleos.net>
2021-03-26 17:30:38 +0100

    remove "app" folder

--- app/lib/mediapipe/geometry.js
+++
@@ -1,6 +0,0 @@
-  exports.getDistance = (a, b) => {
-    const distanceRaw = ((a.x - b.x) ** 2 + (a.y - b.y) ** 2) ** 0.5;
-    console.debug("distanceRaw", distanceRaw);
-    const distanceReal = distanceRaw / ((a.z + b.z) / 2);
-    console.debug("distanceReal", distanceReal);
-  }
